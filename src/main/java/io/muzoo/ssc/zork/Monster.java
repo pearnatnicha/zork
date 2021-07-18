28ce5dfc7b5e0c5cd7500dc98e2a8bb2d6a1ea5b
Natnicha Suratniti <pearnatnicha@Macs-MacBook-Air-120.local>
2021-07-18 16:39:15 +0700

    add monster

--- src/main/java/io/muzoo/ssc/zork/Monster.java
+++ src/main/java/io/muzoo/ssc/zork/Monster.java
@@ -1,4 +1,28 @@
 package io.muzoo.ssc.zork;
 
+import java.util.Random;
+
+
 public class Monster {
+    public int HP;
+    private int attackPower;
+
+    public Monster(){
+        HP = 100;
+        Random random = new Random();
+        attackPower = random.nextInt(15);
+    }
+
+    public void getDamage(int damaged){
+        if (HP > 0){
+            HP -= damaged;
+            System.out.println("Monster's HP -" + damaged);
+        }else{
+            System.out.println("Monster is death");
+        }
+    }
+
+    public int monsterAttack(){
+        return attackPower;
+    }
 }
